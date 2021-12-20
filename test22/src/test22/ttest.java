9334198bbd765d95de928f5ef2be026f07e2a306
smhrd <smhrd@DESKTOP-445UFS9>
2021-12-20 17:33:54 +0900

    ㅇㅎ

--- test22/src/test22/ttest.java
+++ test22/src/test22/ttest.java
@@ -5,6 +5,8 @@ public class ttest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("테스트");
+		System.out.println("테스트");
+		System.out.println("테스트");
 	}
 
 }
