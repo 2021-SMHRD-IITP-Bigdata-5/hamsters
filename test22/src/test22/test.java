cf555f9822317d2709bcb2c8d6ebe86f72b6f3d9
smhrd <smhrd@DESKTOP-445UFS9>
2021-12-20 18:44:37 +0900

    jar

---
+++ test22/src/test22/test.java
@@ -0,0 +1,43 @@
+package test22;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class test extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Connection conn = null;
+		PreparedStatement psmt = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName("project-db-stu.ddns.net");
+			System.out.println("클래스파일 로딩완료");
+
+			// 3. DB에서 사용하는 ID/PW를 인증
+			String url = "jdbc:oracle:thin:@127.0.0.1:1524";
+			String dbid = "cgi_7_1_1216";
+			String dbpw = "smhrd1";
+
+			conn = DriverManager.getConnection(url, dbid, dbpw);
+
+			if (conn != null) {
+				System.out.println("연결성공");
+			} else {
+				System.out.println("연결실패");
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
